7d1f1c5dd821d040b6b652700a3b7035bd5afd7b
kira <ouyongyong@163.com>
2012-02-11 18:00:42 +0800

    new proj

---
+++ Shuriken/Shuriken/SKAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SKAppDelegate.h
+//  Shuriken
+//
+//  Created by Orange on 12-2-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Shuriken/ShurikenTests/ShurikenTests.h
@@ -0,0 +1,13 @@
+//
+//  ShurikenTests.h
+//  ShurikenTests
+//
+//  Created by Orange on 12-2-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ShurikenTests : SenTestCase
+
+@end
