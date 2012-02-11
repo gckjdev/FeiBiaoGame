144747b124fda271540cb4da90b7b1522e786e1f
kira <ouyongyong@163.com>
2012-02-11 17:13:29 +0800

    save

---
+++ ShurikenTests/ShurikenTests.m
@@ -0,0 +1,32 @@
+//
+//  ShurikenTests.m
+//  ShurikenTests
+//
+//  Created by Orange on 12-2-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShurikenTests.h"
+
+@implementation ShurikenTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ShurikenTests");
+}
+
+@end
